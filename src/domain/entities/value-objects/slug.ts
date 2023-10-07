41949983e2a56d14debfa754a869a61a5fc68030
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-10-07 14:15:12 -0300

    value object de slug

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,32 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Recebe uma string e normaliza como um slug
+   * 
+   * Exemplo: "Um título de exemplo" => "um-titulo-de-exemplo"
+   * 
+   * @param text 
+   * 
+   */
+
+  static createFromText(text: string) {
+    //a função normalize padroniza a strign removendo os caracteres que não sejam aceitos dentro de uma convenção de caracteres
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+
+}
